ee2406f3b58adacc815adb2b83c72e9be3bdbdf9
feichang <pswho@yahoo.cn>
2012-12-14 13:58:42 +0800

    更新node veryless

--- grunt.js
+++ grunt.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
   	grunt.initConfig({
 		concat: {
 		    veryless: {
-		    	src: ['src/utilities/*', 'src/css3/*', 'src/layout/_grid.less', 'src/layout/_fluid.less', 'src/layout/_metro.less'],
+		    	src: ['src/reset/*', 'src/utilities/*', 'src/css3/*', 'src/layout/_grid.less', 'src/layout/_fluid.less', 'src/layout/_metro.less'],
 		    	dest: 'dist/veryless-1.0.0.less'
 		    },
 		    plugin: {
